52b1f235fb54a9aff0511d462ef89f9a58340a2b
Vo-Ngoc-Khanh <khanhvo02909@gmail.com>
2022-03-04 19:55:30 +0700

    guide.dart finished

--- fluttter_quiz_app/lib/guide.dart
+++ fluttter_quiz_app/lib/guide.dart
@@ -17,11 +17,11 @@ class GuidePage extends StatelessWidget {
             child: Card(
               child: SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(40.0),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('Trắc nghiệm tính cách',
+                        const Text('Trắc nghiệm tính cách MBTI',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
@@ -29,23 +29,27 @@ class GuidePage extends StatelessWidget {
                         ),
                         sizedBox(10.0),
                         textGuide(
-                            'Bài trắc nghiệm này gồm 70 câu hỏi ngắn về chính bản thân bạn.Bạn có 10 phút để làm bài '),
+                            'Bài trắc nghiệm này gồm 70 câu hỏi ngắn về chính bản thân bạn.Bạn có 10 phút để làm bài. Thông thường chỉ mất khoảng 7 phút để hoàn thành. '),
                         sizedBox(10.0),
                         textGuide(
                             'Sau khi hoàn thành bài trắc nghiệm bạn sẽ nhận được kết quả thuộc 1 trong 16 nhóm tính cách sau :'),
                         sizedBox(10.0),
                         Image.asset('assets/images/mbti.png'),
                         sizedBox(10.0),
-                        textGuide(
-                            'Để bài trắc nghiệm cho kết quả đúng nhất, bạn nên :'),
+                        const Text('Lưu ý:',style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),),
                         sizedBox(10.0),
-                        textGuide('- Trả lời thành thật.'),
-                        sizedBox(4.0),
+                        textGuide('Kết quả của một trắc nghiệm tâm lý phụ thuộc rất nhiều vào tâm trạng của bạn. Tốt nhất hãy thực hiện nó trong một trạng thái tâm lý bình ổn nhất. Nếu bạn đang quá vui, buồn, phấn khích, bực bội hay đang trong quá trình thay đổi nhận thức thì sẽ không đảm bảo được độ chính xác.'),
+                        sizedBox(10.0),
                         textGuide(
-                            '- Trả lời theo bản thân mình hiện tại, không phải theo ước vọng mình muốn.'),
-                        sizedBox(4.0),
+                            'Trung thực khi trả lời câu hỏi, phân biệt giữa lý tưởng và thực tế. Kết quả của trắc nghiệm hoàn toàn là câu chuyện cá nhân của bạn, đừng để yếu tố bên ngoài tác động đến câu trả lời.'),
+                        sizedBox(10.0),
                         textGuide(
-                            '- Câu trả lời theo trực giác ban đầu chính xác nhất'),
+                            'Chúng ta trưởng thành và thay đổi từng ngày, kết quả trắc nghiệm tất nhiên có thể từ đó mà thay đổi tùy theo nhận thức và thế giới quan của mỗi người. Tốt nhất hãy làm bài kiểm tra nhiều lần và điều độ để có cái nhìn tổng quát và chính xác nhất.'),
+                        sizedBox(10.0),
+                        textGuide('Kết quả của bài trắc nghiệm MBTI sau đây cung cấp thông tin để bạn có lựa chọn nghề nghiệp thích hợp dựa trên các nhóm tính cách. Mọi dữ liệu đưa ra đều mang tính chất tham khảo, quyết định luôn ở trong tay chúng ta. '),
                       ]),
                 ),
               ),
@@ -60,8 +64,8 @@ class GuidePage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton.icon(onPressed: (){}, label: const Text("Quay lại"), icon: const Icon(Icons.arrow_back),),
-                  ElevatedButton.icon(onPressed: (){},label: const Text("Tiếp tục"), icon: const Icon(Icons.arrow_forward),),
+                  ElevatedButton.icon(onPressed: (){}, label: const Text("QUAY LẠI"), icon: const Icon(Icons.arrow_back),),
+                  ElevatedButton.icon(onPressed: (){},label: const Text("BẮT ĐÂU"), icon: const Icon(Icons.arrow_forward),),
                 ],
               ),
             ),
