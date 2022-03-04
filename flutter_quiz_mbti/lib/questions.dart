7607c13e426a58d0163d73e524cefd919c83791e
Vo-Ngoc-Khanh <khanhvo02909@gmail.com>
2022-03-04 12:07:59 +0700

    init

---
+++ flutter_quiz_mbti/lib/questions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatelessWidget {
+  const QuestionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
